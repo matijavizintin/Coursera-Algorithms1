9bacc1313c1edc5f465cb0e2ac4cb7787d48cb17
matijavizintin <matija.vizintin@gmail.com>
2015-08-21 19:39:28 +0200

    hash tables

---
+++ src/main/java/w6/SeparateChainingHashST.java
@@ -0,0 +1,63 @@
+package w6;
+
+/**
+ * Created by Matija Vižintin
+ * Date: 10. 08. 2015
+ * Time: 21.06
+ */
+public class SeparateChainingHashST<KEY, VALUE> {
+    private int M;      // chain size
+    private Node[] st;
+
+    public SeparateChainingHashST() {
+        this(97);
+    }
+
+    public SeparateChainingHashST(int m) {
+        M = m;
+        st = new Node[M];
+    }
+
+    private int hash(KEY key) {
+        return (key.hashCode() & 0x7fffffff) % M;
+    }
+
+    public VALUE get(KEY key) {
+        int i = hash(key);
+
+        // go through the linked list at st[i]
+        for(Node x = st[i]; x != null; x = x.next) {
+            if (key.equals(x.key))      // key found, return value
+                return (VALUE)x.value;
+        }
+        return null;        // not found
+    }
+
+    public void put(KEY key, VALUE value) {
+        int i = hash(key);
+
+        // go through the linked list at st[i]
+        for (Node x = st[i]; x != null; x = x.next) {
+            if (key.equals(x.key)) {     // if key found then update the value
+                x.value = value;
+                return;
+            }
+        }
+
+        // key was not in the table, add the element in the front of the linked list at st[i]
+        st[i] = new Node(key, value, st[i]);
+    }
+
+    // linked list representation with a node
+    private static class Node {
+        private Object key;
+        private Object value;
+        private Node next;
+
+        public Node(Object key, Object value, Node next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+    }
+}
---
+++ src/test/java/w6/SeparateChainingHashSTTest.java
@@ -0,0 +1,169 @@
+package w6;
+
+import org.junit.Assert;
+import org.junit.Test;
+import w4.SortedSymbolicTable;
+
+import java.time.Clock;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+/**
+ * Created by Matija Vižintin
+ * Date: 11. 08. 2015
+ * Time: 22.55
+ */
+public class SeparateChainingHashSTTest {
+
+    @Test
+    public void test() {
+        SeparateChainingHashST<Integer, String> hashTable = new SeparateChainingHashST<>();
+
+        hashTable.put(1, "a");
+        hashTable.put(2, "b");
+        hashTable.put(3, "c");
+        hashTable.put(4, "d");
+        hashTable.put(5, "e");
+        hashTable.put(6, "f");
+        hashTable.put(7, "g");
+
+        String value = hashTable.get(1);
+        Assert.assertEquals("a", value);
+
+        value = hashTable.get(2);
+        Assert.assertEquals("b", value);
+
+        value = hashTable.get(3);
+        Assert.assertEquals("c", value);
+
+        value = hashTable.get(4);
+        Assert.assertEquals("d", value);
+
+        value = hashTable.get(5);
+        Assert.assertEquals("e", value);
+
+        value = hashTable.get(6);
+        Assert.assertEquals("f", value);
+
+        value = hashTable.get(7);
+        Assert.assertEquals("g", value);
+    }
+
+    @Test
+    public void test1k() {
+        Instant start = Clock.systemUTC().instant();
+
+        SeparateChainingHashST<Integer, Integer> hashTable = new SeparateChainingHashST<>(200);
+        for (int i = 0; i < 1000; i++) {
+            hashTable.put(i, i);
+        }
+
+        for (int i = 0; i < 1000; i++) {
+            int value = hashTable.get(i);
+            Assert.assertEquals(value, i);
+        }
+
+        Instant stop = Clock.systemUTC().instant();
+        long elapsed = ChronoUnit.MILLIS.between(start, stop);
+        System.out.println(elapsed);
+    }
+
+    @Test
+    public void test100k() {
+        Instant start = Clock.systemUTC().instant();
+
+        SeparateChainingHashST<Integer, Integer> hashTable = new SeparateChainingHashST<>(20000);
+        for (int i = 0; i < 100000; i++) {
+            hashTable.put(i, i);
+        }
+
+        for (int i = 0; i < 100000; i++) {
+            int value = hashTable.get(i);
+            Assert.assertEquals(value, i);
+        }
+
+        Instant stop = Clock.systemUTC().instant();
+        long elapsed = ChronoUnit.MILLIS.between(start, stop);
+        System.out.println(elapsed);
+    }
+
+    @Test
+    public void test1M() {
+        Instant start = Clock.systemUTC().instant();
+
+        int size = 1000 * 1000;
+        int hashSize = 1000 * 300;
+
+        SeparateChainingHashST<Integer, Integer> hashTable = new SeparateChainingHashST<>(hashSize);
+        for (int i = 0; i < size; i++) {
+            hashTable.put(i, i);
+        }
+
+        for (int i = 0; i < size; i++) {
+            int value = hashTable.get(i);
+            Assert.assertEquals(value, i);
+        }
+
+        Instant stop = Clock.systemUTC().instant();
+        long elapsed = ChronoUnit.MILLIS.between(start, stop);
+        System.out.println(elapsed);
+    }
+
+    @Test
+    public void test1kSST() {
+        Instant start = Clock.systemUTC().instant();
+
+        SortedSymbolicTable<Integer, Integer> hashTable = new SortedSymbolicTable<>();
+        for (int i = 0; i < 1000; i++) {
+            hashTable.put(i, i);
+        }
+
+        for (int i = 0; i < 1000; i++) {
+            int value = hashTable.get(i);
+            Assert.assertEquals(value, i);
+        }
+
+        Instant stop = Clock.systemUTC().instant();
+        long elapsed = ChronoUnit.MILLIS.between(start, stop);
+        System.out.println(elapsed);
+    }
+
+    @Test
+    public void test100kSST() {
+        Instant start = Clock.systemUTC().instant();
+
+        SortedSymbolicTable<Integer, Integer> hashTable = new SortedSymbolicTable<>();
+        for (int i = 0; i < 100000; i++) {
+            hashTable.put(i, i);
+        }
+
+        for (int i = 0; i < 100000; i++) {
+            int value = hashTable.get(i);
+            Assert.assertEquals(value, i);
+        }
+
+        Instant stop = Clock.systemUTC().instant();
+        long elapsed = ChronoUnit.MILLIS.between(start, stop);
+        System.out.println(elapsed);
+    }
+
+    @Test
+    public void test1MSST() {
+        Instant start = Clock.systemUTC().instant();
+
+        int size = 1000 * 1000;
+        SortedSymbolicTable<Integer, Integer> hashTable = new SortedSymbolicTable<>();
+        for (int i = 0; i < size; i++) {
+            hashTable.put(i, i);
+        }
+
+        for (int i = 0; i < size; i++) {
+            int value = hashTable.get(i);
+            Assert.assertEquals(value, i);
+        }
+
+        Instant stop = Clock.systemUTC().instant();
+        long elapsed = ChronoUnit.MILLIS.between(start, stop);
+        System.out.println(elapsed);
+    }
+}
